e34c4c0fd5fcf68cc6aa1d3a21e7a4ca0b969811
wagnermarques <wagnerdocri@gmail.com>
2019-04-22 19:53:25 -0300

    firebase auth

--- projetos/cordovafirebase_emailauth/www/js/index.js
+++ projetos/cordovafirebase_emailauth/www/js/index.js
@@ -22,24 +22,8 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onDeviceReady: function() {        
+        onLoadFunction();
     }
 };
 
